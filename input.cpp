fbd24458cb00b96d0bfc39ce0380837fdba9bdf2
Luciano Cauzzi <cauzzi.luciano@liceoamaldi.edu.it>
2022-11-15 08:53:28 +0100

    Initial commit

---
+++ input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Input" << endl;
+    int num;
+    cout << "Inserisci un numero intero " << endl;
+    cin >> num;
+    cout << "Num vale " << num << endl;
+    return 0;
+}
---
+++ multiple_input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Input multipli" << endl;
+    float num1, num2;
+    cout << "Inserisci due numeri " << endl;
+    cin >> num1 >> num2;
+    cout << "Il prodotto vale " << num1 * num2 << endl;
+    return 0;
+}
